efa71bc7704aacf2031139deb22c0a670b5d26b3
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2025-07-28 19:37:21 +0530

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://mortal22soul.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
 });
