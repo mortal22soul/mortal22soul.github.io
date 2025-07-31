083a306be98a19a02f04a36ca0efd48af7a908fe
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2025-07-31 12:12:29 +0530

    set output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  output: "static",
   site: "https://mortal22soul.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
 });
